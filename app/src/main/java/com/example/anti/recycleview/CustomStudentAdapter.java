d586e0abad9e048a8cbe9ccb2b97a17ffad24420
Anti <pane.dimovski@gmail.com>
2017-12-14 20:27:59 +0100

    Nov Adapter Image so Json i Gson , i imageModel, i Interfejs onClickListener.

--- app/src/main/java/com/example/anti/recycleview/CustomStudentAdapter.java
+++ app/src/main/java/com/example/anti/recycleview/CustomStudentAdapter.java
@@ -1,11 +1,16 @@
 package com.example.anti.recycleview;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +26,20 @@ public class CustomStudentAdapter extends RecyclerView.Adapter<CustomStudentAdap
 
     List<Student> studentList = new ArrayList<>();
 
+    Context context;
+
+    public CustomStudentAdapter(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+    public  CustomStudentAdapter (Context context ){
+        this.context = context;
+
+    }
+    public void setItems (List<Student>   students  )  {
+
+        studentList = students;
+
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -31,7 +50,6 @@ public class CustomStudentAdapter extends RecyclerView.Adapter<CustomStudentAdap
         View view = inflater.inflate(R.layout.novlayout, parent, false);
         // Return a new holde instance
         ViewHolder viewHolder = new ViewHolder(view);
-
 
         return viewHolder;
     }
@@ -45,6 +63,20 @@ public class CustomStudentAdapter extends RecyclerView.Adapter<CustomStudentAdap
 
         holder.textView.setText(student.getsName());
 
+
+
+        if (student.issOnline()){
+            holder.txt2.setText("Is Online");
+            holder.txt2.setTextColor(Color.BLUE);
+
+
+        }
+        else {
+
+            holder.txt2.setText("Is Off line");
+            holder.txt2.setBackgroundColor(Color.RED);
+        }
+        Picasso.with(context).load("http://www.hellomaps.net/images/joomla-maps/faceuser1.jpg").centerInside().fit().into(holder.slika);
     }
 
     @Override
@@ -56,6 +88,12 @@ public class CustomStudentAdapter extends RecyclerView.Adapter<CustomStudentAdap
 
         @BindView(R.id.text)
         TextView textView;
+
+        @BindView(R.id.text2)
+        TextView txt2;
+
+        @BindView(R.id.slika)
+        ImageView slika;
 
         public ViewHolder(View itemView) {
             super(itemView);
--- app/src/main/java/com/example/anti/recycleview/MainActivity.java
+++ app/src/main/java/com/example/anti/recycleview/MainActivity.java
@@ -2,10 +2,19 @@ package com.example.anti.recycleview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.recycleView)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +22,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+
+       CustomStudentAdapter adapter = new CustomStudentAdapter( this);
+
+       adapter.setItems(generateList());
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
+    ArrayList<Student> generateList (){
+
+        ArrayList<Student> students = new ArrayList<>();
+         students.add(new Student("Student prv", false) );
+        students.add(new Student("Student vtor", true) );
+        students.add(new Student("Student tret", false) );
+
+
+        for (int i = 0; i <100; i++)
+        {
+
+            students.add(new Student("Student " + i, true) );
+        }
+
+        return students;
 
     }
 }
